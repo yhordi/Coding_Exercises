735d639b7cd748d82aad90f836f513ffa570c406
Jordan Kamin <Jordan@Jordans-MacBook-Air.local>
2014-10-01 18:01:47 -0700

    drag functionality functional

--- DragAndDrop/app.js
+++ DragAndDrop/app.js
@@ -5,6 +5,19 @@ $(document).ready(function() {
   // $(init);
   //   function init() {
     $('#drag').draggable();
+
+    $('#drop').droppable({
+      accept: '#drag',
+      activate: function( event, ui ) {}
+    });
+    $('#drop').on('dropover', function(e, ui){
+      $(this).css("border", "5px solid lime"),
+      $(this).html("<p>Like a 'MERICAN!</p>")
+    } )
+    $('#drop').on('dropout', function(e, ui){
+      $(this).css("border", "1px solid black")
+      $(this).html("NOT A COMMUNIST.")
+    })
   // }
 });
 
