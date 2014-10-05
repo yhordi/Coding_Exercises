1ad69d3ce79e59285623fde0e87f7e29e1546750
Jordan Kamin <Jordan@Jordans-MacBook-Air.local>
2014-10-05 14:39:43 -0700

    ajax request not working. working on CORS request

--- ajax-practice/script.js
+++ ajax-practice/script.js
@@ -1,21 +1,29 @@
 // Steam api Key: 7EE993F9E83B5EB4599758D53E0074F1
 $(document).ready(function(){
  var steam = new Steam
- steam.getId()
+ steam.getStuff()
 });
 var Steam = function(){
 }
 
 Steam.prototype = {
-  getId: function(){
-    // this.createCORSRequest()
+  getStuff: function(){
+    this.createCORSRequest()
+    this.makeCORSRequest()
     $.ajax({
+      dataType: 'Json',
+      type: 'get',
       url: "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/",
       data: {
-        key: '7EE993F9E83B5EB4599758D53E0074F1',
-        steamids: '76561197985852059'
+        key: 'E6B0ADB9B2C8C41CC3A0D86C3F235406',
+        steamids: '76561197985852059',
       },
-      dataType: 'get',
+      xhrFields: {
+        withCredentials: false
+      },
+      headers: {
+        'Access-Control-Allow-Headers': true
+      },
       success: function (data) {
         $.parseJSON (data);
         response = data;
@@ -30,26 +38,31 @@ Steam.prototype = {
       }
     })
   },
-  // createCORSRequest: function(method, url) {
-  //   var xhr = new XMLHttpRequest();
-  //   if ("withCredentials" in xhr) {
-  //   // Check if the XMLHttpRequest object has a "withCredentials" property.
-  //   // "withCredentials" only exists on XMLHTTPRequest2 objects.
-  //   xhr.open(method, url, true);
-  // } else if (typeof XDomainRequest != "undefined") {
-  //   // Otherwise, check if XDomainRequest.
-  //   // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-  //   xhr = new XDomainRequest();
-  //   xhr.open(method, url);
-  // } else {
-  //   // Otherwise, CORS is not supported by the browser.
-  //   xhr = null;
-  // }
-  // return xhr;
-  // },
-
-  // var xhr = createCORSRequest('GET', url);
-  // if (!xhr) {
-  //   throw new Error('CORS not supported');
-  // },
+  createCORSRequest: function(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        xhr = null;
+    }
+    return xhr;
+  },
+  makeCORSRequest: function() {
+    var url = 'http://api.steampowered.com';
+    var xhr = this.createCORSRequest('GET', url);
+    if (!xhr) {
+        alert('CORS not supported');
+        return;
+    }
+    xhr.onload = function() {
+      var text = xhr.responseText;
+    };
+    xhr.onerror = function() {
+      console.log('Woops, there was an error making the request.');
+    };
+    xhr.send();
+  }
 }
