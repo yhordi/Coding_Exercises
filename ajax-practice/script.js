ddd8661a4b3a93ac7c9f40fbd51ae7b470a8f184
Jordan Kamin <Jordan@Jordans-MacBook-Air.local>
2014-10-05 23:55:45 -0700

    got the ajax working

--- ajax-practice/script.js
+++ ajax-practice/script.js
@@ -8,26 +8,21 @@ var Steam = function(){
 
 Steam.prototype = {
   getStuff: function(){
-    this.createCORSRequest()
-    this.makeCORSRequest()
+    // this.createCORSRequest()
+    // this.makeCORSRequest()
     $.ajax({
       dataType: 'Json',
       type: 'get',
+      useDefaultXhrHeader: false,
       url: "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/",
       data: {
         key: 'E6B0ADB9B2C8C41CC3A0D86C3F235406',
         steamids: '76561197985852059',
       },
-      xhrFields: {
-        withCredentials: false
-      },
-      headers: {
-        'Access-Control-Allow-Headers': true
-      },
       success: function (data) {
-        $.parseJSON (data);
         response = data;
         console.log('SUCCESS!', response, data);
+        $.parseJSON (data);
       },
       error: function (bug) {
         console.log(bug)
@@ -38,31 +33,31 @@ Steam.prototype = {
       }
     })
   },
-  createCORSRequest: function(method, url) {
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr) {
-        xhr.open(method, url, true);
-    } else if (typeof XDomainRequest != "undefined") {
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-    } else {
-        xhr = null;
-    }
-    return xhr;
-  },
-  makeCORSRequest: function() {
-    var url = 'http://api.steampowered.com';
-    var xhr = this.createCORSRequest('GET', url);
-    if (!xhr) {
-        alert('CORS not supported');
-        return;
-    }
-    xhr.onload = function() {
-      var text = xhr.responseText;
-    };
-    xhr.onerror = function() {
-      console.log('Woops, there was an error making the request.');
-    };
-    xhr.send();
-  }
+  // createCORSRequest: function(method, url) {
+  //   var xhr = new XMLHttpRequest();
+  //   if ("withCredentials" in xhr) {
+  //       xhr.open(method, url, true);
+  //   } else if (typeof XDomainRequest != "undefined") {
+  //       xhr = new XDomainRequest();
+  //       xhr.open(method, url);
+  //   } else {
+  //       xhr = null;
+  //   }
+  //   return xhr;
+  // },
+  // makeCORSRequest: function() {
+  //   var url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/';
+  //   var xhr = this.createCORSRequest('GET', url);
+  //   if (!xhr) {
+  //       alert('CORS not supported');
+  //       return;
+  //   }
+  //   xhr.onload = function() {
+  //     var text = xhr.responseText;
+  //   };
+  //   xhr.onerror = function() {
+  //     console.log('Woops, there was an error making the request.');
+  //   };
+  //   xhr.send();
+  // }
 }
