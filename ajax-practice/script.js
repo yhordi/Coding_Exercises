ac96f4d92f091bf0ab2a352693e21782d0f37553
Jordan Kamin <Jordan@Jordans-MacBook-Air.local>
2014-10-08 10:06:52 -0700

    able to access response data

--- ajax-practice/script.js
+++ ajax-practice/script.js
@@ -1,27 +1,25 @@
 $(document).ready(function(){
  var steam = new Steam
- var ajaxData
- steam.getData()
+ steam.ajaxRequest()
 });
+ var ajaxData = null;
+
 var Steam = function(){
 }
 
 Steam.prototype = {
-  getData: function(){
+  ajaxRequest: function(){
     $.ajax({
       dataType: 'Json',
       type: 'get',
-      useDefaultXhrHeader: false,
       url: "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/",
       data: {
         key: 'E6B0ADB9B2C8C41CC3A0D86C3F235406',
         steamids: '76561197985852059',
       },
       success: function (data) {
-        ajaxData = data;
+        ajaxData = data
         console.log('SUCCESS!');
-        responseData = $.parseJSON (data);
-
       },
       error: function (bug) {
         console.log(bug)
@@ -32,9 +30,9 @@ Steam.prototype = {
       }
     })
   },
+  injectResponse: function() {
+    $(".username").html(ajaxData.response.players[0].realname)
+  }
 }
 
-var dataParse = function(){
-  $(".username").append(ajaxData.response.players[0].realname)
-}
 
