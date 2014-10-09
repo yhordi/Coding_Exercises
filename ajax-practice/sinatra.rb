f91738c74d6ae3523d477e91957dd8c3f73f64bd
Jordan Kamin <Jordan@Jordans-MacBook-Air.local>
2014-10-09 13:09:33 -0700

    getting response with HTTParty

--- ajax-practice/sinatra.rb
+++ ajax-practice/sinatra.rb
@@ -3,9 +3,10 @@ require 'sinatra'
 require 'HTTParty'
 
 # set :public_folder, File.dirname(__FILE__) + '/public'
+response = HTTParty.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=mysteamkeynotreallytho&steamids=76561197985852059")
 
+puts response
 get '/' do
   File.open("index.html")
-  # File.read(File.join('public', 'index.html'))
-end
 
+end
